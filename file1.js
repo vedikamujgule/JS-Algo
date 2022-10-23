2c661b456985c506947f5824f8901a7cec61a832
Vedika Mujgule <vedika.mujgule@mediaagility.com>
2022-10-23 18:38:24 +0530

    time complexity

--- file1.js
+++ file1.js
@@ -3,8 +3,11 @@ let finbonacci = [0,1];
 
 function finbonacciSeries(n){
     for(let i=2; i<n; i++)
-    finbonacci[i] = finbonacci[i-1]+finbonacci[i-2]
+    finbonacci[i] = finbonacci[i-1]+finbonacci[i-2]    // ==> O(n)
     return finbonacci;
 }
 
 console.log(finbonacciSeries(7));
+
+//Time conplexity
+// Big(O) = O(n)
